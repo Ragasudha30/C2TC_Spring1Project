59e6761b6cd1f03eb6be97c9d86966ca55db9c52
HP <HP@Sudha.bbrouter>
2025-08-10 12:28:24 +0530

    SpringBoot project 1

--- placement-management/src/main/java/com/tnsif/placementmanagement/PlacementManagementApplication.java
+++ placement-management/src/main/java/com/tnsif/placementmanagement/PlacementManagementApplication.java
@@ -3,6 +3,7 @@ package com.tnsif.placementmanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PlacementManagementApplication {
 
